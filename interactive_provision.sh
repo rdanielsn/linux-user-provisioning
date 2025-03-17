65dda74f30c3459e3bf40f485b9b4b1f8e6f1f87
rdanielsn <10221010@mahasiswa.itb.ac.id>
2025-03-17 15:01:34 +0700

    feat: add create new user with manual password - automatically create home directory for new user

--- interactive_provision.sh
+++ interactive_provision.sh
@@ -2,5 +2,14 @@
 
 function interactive() {
     echo ""
-    echo "You've choosen interactive user creation."
+    echo "Interactive User Creation"
+    echo ""
+
+    read -rp "Enter full name: " FULLNAME
+    read -rp "Enter new username: " NEWUSER
+
+    sudo useradd -m -c "$FULLNAME" $NEWUSER
+    sudo passwd $NEWUSER
+
+    echo "Successfully create user: $NEWUSER"
 }
