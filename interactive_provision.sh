79bc3ef30cf53372edee83196309c152b5620c57
rdanielsn <10221010@mahasiswa.itb.ac.id>
2025-03-21 01:01:55 +0000

    feat: add check if username exist

--- interactive_provision.sh
+++ interactive_provision.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
+function check_if_user_exist() {
+    if id -u "$1" >/dev/null 2>&1; then
+        echo 1
+    else
+        echo 0
+    fi
+}
+
 function interactive() {
     echo ""
     echo "Interactive User Creation"
     echo ""
 
     read -rp "Enter full name: " FULLNAME
-    read -rp "Enter new username: " NEWUSER
 
-    sudo useradd -m -c "$FULLNAME" $NEWUSER
-    sudo passwd $NEWUSER
+    while true; do
+        read -rp "Enter new username: " NEWUSER
 
-    echo "Successfully create user: $NEWUSER"
+        USER_EXIST=$(check_if_user_exist $NEWUSER)
+
+        if [[ "$USER_EXIST" -eq 0 ]]; then
+            echo ""
+            sudo useradd -m -c "$FULLNAME" $NEWUSER
+            sudo passwd $NEWUSER
+
+            echo ""
+            echo "Successfully create user: $NEWUSER"
+            echo ""
+            break
+        else
+            echo "Error: User '$NEWUSER' already exists."
+            echo ""
+        fi
+    done
 }
